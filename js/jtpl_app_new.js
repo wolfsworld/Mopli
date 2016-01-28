26b2ec35989cc225d9a49c16c12bd768764a934d
wolfsworld <wolf@wolfsworld1.com>
2016-01-28 07:44:33 -0500

    mjmj

--- js/jtpl_app_new.js
+++ js/jtpl_app_new.js
@@ -613,6 +613,7 @@ $('.trail a').button('refresh');
 });
 
 $( "#blist" ).append(blist_html);
+$( "#thecount" ).append(''+i+',');
 $('.trail a').button();
 if(page_counter==1){
 next_batch +="<a href='#' id='fwd_btn' class='ui-btn ui-corner-all ui-icon-cloud ui-btn-icon-left'>...next 20 results</a>";
