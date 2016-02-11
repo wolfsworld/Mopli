7af93ddbf90e788524259f8c8c8a4c4cd1b868c7
wolfsworld <wolf@wolfsworld1.com>
2016-02-11 10:11:02 -0500

    hgf

--- js/jtpl_app.js
+++ js/jtpl_app.js
@@ -351,7 +351,7 @@ $('#cn_holdreq').val("");
 
 //Home Button workaround to work with IOS 9
 $('.home').on('click', function () {
-refresh_ct++;
+refresh_ct=refresh_ct+1;
 alert(refresh_ct);
 self.location.href = 'index.html';
 });
