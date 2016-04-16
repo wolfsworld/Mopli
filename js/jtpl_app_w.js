77bb2d1ea48ecf7a27b3dd4f89dd9cd31a5acd92
wolfsworld <wolf@wolfsworld1.com>
2016-04-16 09:24:22 -0400

    hgjhgh

--- js/jtpl_app_w.js
+++ js/jtpl_app_w.js
@@ -429,7 +429,7 @@ $.ajax({
         type: "POST",
 		async: true,
 		//dataType: "json",
-		url: "http://jeffersonlibrary.net/MOPLI/INTERMED_short_test.php",
+		url: "http://jeffersonlibrary.net/MOPLI/INTERMED_short.php",
         crossDomain: true,
         data: {"uri": ""+reqstring+"", "rdate": ""+thedate+"", "method":""+p_method+"", "patron_pin":""+p_pwd+""},
 		error: function(jqXHR,text_status,strError){
