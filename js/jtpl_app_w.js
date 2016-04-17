b1b73596ea3441fecd2bee8fdaa232d26a1ca8b2
wolfsworld <wolf@wolfsworld1.com>
2016-04-17 09:41:21 -0400

    eeee

--- js/jtpl_app_w.js
+++ js/jtpl_app_w.js
@@ -1670,7 +1670,7 @@ start_spin();
 function most_popular(code,reqstring,thedate){
 //alert('thedate: '+thedate+' - code: '+code+' - reqstring: '+reqstring+'');
 var code="PWS WebDev:u2pjMOAcp2yPyziu9ZAd3y4o2a8=";
-var thedate="Sun, 17 Apr 2016 13:30:20";
+var thedate="Sun, 17 Apr 2016 13:30:20 GMT";
 var reqstring="https://catalog.mainlib.org/PAPIService/REST/public/v1/1033/100/1/search/bibs/boolean?q=COL=7+sortby+MP/sort.descending&page=1";
 
 var settings = {
