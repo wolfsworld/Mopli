4fc12c0cc8b87492167c4f16428f627c0de33e22
wolfsworld <wolf@wolfsworld1.com>
2016-04-17 09:36:01 -0400

    jkjkj

--- js/jtpl_app_w.js
+++ js/jtpl_app_w.js
@@ -1668,7 +1668,11 @@ start_spin();
 });
 //case 12 - list most popular
 function most_popular(code,reqstring,thedate){
-alert('thedate: '+thedate+' - code: '+code+' - reqstring: '+reqstring+'');
+//alert('thedate: '+thedate+' - code: '+code+' - reqstring: '+reqstring+'');
+var code="PWSWebDev:u2pjMOAcp2yPyziu9ZAd3y4o2a8=";
+var thedate="Sun, 17 Apr 2016 13:30:20";
+var reqstring="https://catalog.mainlib.org/PAPIService/REST/public/v1/1033/100/1/search/bibs/boolean?q=COL=7+sortby+MP/sort.descending&page=1";
+
 var settings = {
   "async": true,
   "crossDomain": true,
