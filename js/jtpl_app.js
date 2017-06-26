39a1c643be9719d61dcffe4784537c71bf827e53
Wolf <WOLF@WOLFs-MacBook-Pro.local>
2017-06-26 13:52:58 -0400

    qeqeq

--- js/jtpl_app.js
+++ js/jtpl_app.js
@@ -1355,7 +1355,7 @@ $.ajax({
 			var code=response;
 			p_response={"code": ""+code+"", "reqstring": ""+reqstring+"", "thedate": ""+thedate+""};
 			//filter_holds(p_response.code,p_response.reqstring,p_response.thedate,bib_bc,init_key,init_value,media,ISBN);
-			hold_ind=filter_holds1(p_response.code,p_response.reqstring,p_response.thedate);
+			filter_holds1(p_response.code,p_response.reqstring,p_response.thedate);
         },
         error      : function() {
             console.error("error");
@@ -1402,7 +1402,7 @@ alert('tit:'+ title + 'in:' + sys_items_in + 'hold:' + cur_hold_req + 'ind:'+hol
 };//filter_holds1
 	
 }
-
+alert(hold_ind);
 	//alert('RENLEFT:' + RENLEFT + 'hold_ind:' + hold_ind);
 	//alert('it made it to final query');
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
